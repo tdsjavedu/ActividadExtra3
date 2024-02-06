f556a61a649611e1fb81bbfef6010c1039908b2c
Javier Osorio <24004208@galileo.edu>
2024-02-05 23:50:08 -0600

    first commit

---
+++ Unidad II/ActividadJS_24004208.js
@@ -0,0 +1,38 @@
+var mes = prompt("Ingrese mes de nacimiento");
+var dia = prompt("Ingrese dia de nacimiento");
+if(mes == 11 && dia >= 22 || mes == 12 && dia <21){
+  alert("Es Sagitario")
+  }
+if(mes == 12 && dia >= 22 || mes == 1 && dia <=19){
+	alert("Es Capricornio")
+  }
+if(mes == 1 && dia >= 20 || mes == 2 && dia <=18){
+	alert("Es Acuario")
+  }
+if(mes == 2 && dia >=19 || mes == 3 && dia <=20){
+	alert("Es Piscis")
+}
+if(mes == 3 && dia >= 21 || mes == 4 && dia <= 19){
+	alert("Es Aries")
+}
+if(mes == 4 && dia >= 20 || mes == 5 && dia <=20){
+	alert("Es Tauro")
+}
+if(mes == 5 && dia >=21 || mes == 6 && dia <=20 ){
+	alert("Es Geminis")
+}
+if(mes == 6 && dia >= 21 || mes == 7 && dia <= 22){
+	alert("Es Cancer")
+}
+if(mes == 7 && dia >= 23 || mes == 8 && dia <= 22){
+	alert("Es Leo")
+}
+if(mes == 8 && dia >=23 || mes == 9 && dia <= 22){
+	alert("Es Virgo")
+}
+if(mes == 9 && dia >= 23 || mes == 10 && dia <= 22){
+	alert("Es Libra")
+}
+if(mes == 10 && dia >= 23 || mes == 11 && dia <=21){
+	alert("Es Escorpion")
+}
